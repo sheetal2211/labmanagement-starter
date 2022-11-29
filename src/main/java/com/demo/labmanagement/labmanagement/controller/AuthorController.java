251ecec97537eeed37c940d538bc2c410a22de5c
sheetal2211 <36146595+sheetal2211@users.noreply.github.com>
2022-11-29 15:22:53 +0530

    add crud operations for category and author

---
+++ src/main/java/com/demo/labmanagement/labmanagement/controller/AuthorController.java
@@ -0,0 +1,41 @@
+package com.demo.labmanagement.labmanagement.controller;
+
+import com.demo.labmanagement.labmanagement.entity.Author;
+import com.demo.labmanagement.labmanagement.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/author")
+public class AuthorController {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthor(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(authorRepository.findOneById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> saveAuthor(@RequestBody Author author) {
+        return new ResponseEntity<>(authorRepository.save(author), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAuthor(@PathVariable("id") Long id) {
+        authorRepository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthor(@PathVariable("id") Long id, @RequestBody Author author) {
+        author.setId(id);
+        return new ResponseEntity<>(authorRepository.save(author), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/demo/labmanagement/labmanagement/controller/CategoryController.java
@@ -0,0 +1,40 @@
+package com.demo.labmanagement.labmanagement.controller;
+
+import com.demo.labmanagement.labmanagement.entity.Category;
+import com.demo.labmanagement.labmanagement.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @PostMapping
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(categoryRepository.findOneById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable("id") Long id) {
+        categoryRepository.deleteById(id);
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Long id, @RequestBody Category category) {
+        category.setId(id);
+        return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.OK);
+    }
+}
